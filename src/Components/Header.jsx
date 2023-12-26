c2196545f5f7fc4da7e4e6fade34bf0340e548f8
sucaad salaad <SucaadSalaad520@gmail.com>
2023-12-26 05:39:23 -0800

    some changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className="bg-purple-500 p-4 text-white ">
-      <h1 className='text-5xl border-gray-200'> Social Media</h1>
+      <h1 className='md:text-5xl text-2xl border-gray-200'> Social Media</h1>
     </div>
   )
 }
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,6 +1,6 @@
 function Post(props) {
     return (
-      <div className="flex justify-center">
+      <div className="flex justify-center m-7">
         <div className="border-2 mt-4  border-gray-500 p-2">
        <img className="w-[400px]"  src={props.image}/>
        <h1>{props.title}</h1>
